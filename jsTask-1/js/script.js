c30d0c8f4f290e2d1a85ad4bf345475da0cd17c0
vovgreba <49303544+vovgreba@users.noreply.github.com>
2022-05-30 00:17:27 +0300

    feat: added new algorithm

--- jsTask-1/js/script.js
+++ jsTask-1/js/script.js
@@ -1,8 +1,36 @@
-const title = document.querySelector('.title');
-const input = document.querySelector('input[name = "text"]')
+const algorithm = document.querySelector('select[name="algorithm"]');
+let blockAlgorithm = document.querySelector('.listAlgorithm');
+let step = 'one';
 
-input.addEventListener('change', el => {
-    const output = el.target.value;
-    title.textContent = output;
-})
+const getAlgorithm = () => {
+    const result = algorithm.value;
+    const boolen = result === step;
+    if(boolen) {
+        switch(step) {
+            case 'one':
+                blockAlgorithm.insertAdjacentHTML('afterbegin', '<li>Подойти к холодильнику</li>');
+                step = 'two';
+                return;
+            case 'two':
+                blockAlgorithm.insertAdjacentHTML('beforeEnd', '<li>Открыть дверь</li>');
+                step = 'three';
+                return;
+            case 'three':
+                blockAlgorithm.insertAdjacentHTML('beforeEnd', '<li>Достать колбасу</li>');
+                step = 'four';
+                return;
+            case 'four':
+                blockAlgorithm.insertAdjacentHTML('beforeEnd', '<li>Закрыть дверь</li>');
+                return;
+            default:
+                console.error('Что то не то. Попробуйте заново.');
+                
+        }
+    } else {
+        blockAlgorithm.insertAdjacentHTML('beforeEnd', '<li>Неправельние действие в алгоритме. Попробуйте заново.</li>');
+    }
+    
+}
+
+algorithm.addEventListener('change', getAlgorithm);
 
